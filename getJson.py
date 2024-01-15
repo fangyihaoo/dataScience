cdf75ecd4495c457d1dd30ec5d47f4283f74ff78
fangyihaoo <fangyihao116@gmail.com>
2024-01-15 17:04:52 -0600

    read json from url to pd

--- getJson.py
+++ getJson.py
@@ -1,2 +1,18 @@
-import os
-import json
+import requests
+import pandas as pd
+
+def get_json(url):
+    response = requests.get(url)
+    print(type(response))
+    data_json = response.json()  # Parses the JSON data
+    print(type(data_json))  # Prints "<class 'list'>"
+
+    # Create a DataFrame from the JSON data
+    data_new = pd.DataFrame(data_json)
+
+    print(data_new.head())  # Print the first few rows of the DataFrame
+    return data_new
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/posts'
+    get_json(url)
